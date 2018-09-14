89b1bcf509470cbd331f11d78db29f6c8a7b2316
Sergii Leshchenko <sleshche@redhat.com>
2018-09-14 17:05:47 +0300

    Add publishing of STARTED event

--- broker/broker.go
+++ broker/broker.go
@@ -35,11 +35,13 @@ var (
 
 // Start executes plugins metas processing and sends data to Che master
 func Start(metas []model.PluginMeta) {
-	if ok, status := storage.SetStatus(model.StatusStarting); !ok {
+	if ok, status := storage.SetStatus(model.StatusStarted); !ok {
 		m := fmt.Sprintf("Starting broker in state '%s' is not allowed", status)
 		pubFailed(m)
 		log.Fatal(m)
 	}
+	log.Print("Sending STARTING event")
+	pubStarted()
 
 	for _, meta := range metas {
 		err := processPlugin(meta)
@@ -65,6 +67,7 @@ func Start(metas []model.PluginMeta) {
 		pubFailed(err.Error())
 		log.Fatalf(err.Error())
 	}
+	log.Print("Sending Done event")
 	pubDone(string(bytes))
 	closeConsumers()
 }
@@ -80,6 +83,13 @@ func closeConsumers() {
 }
 
 func (tb *tunnelBroadcaster) Close() { tb.tunnel.Close() }
+
+func pubStarted() {
+	bus.Pub(&model.StartedEvent{
+		Status:      model.StatusStarted,
+		WorkspaceID: cfg.WorkspaceID,
+	})
+}
 
 func pubFailed(err string) {
 	bus.Pub(&model.ErrorEvent{
